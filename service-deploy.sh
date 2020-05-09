1670d9f24a12c1add6df362172bd1c8b45a6c3b6
Parallels <parallels@localhost.localdomain>
2020-05-09 10:35:34 -0400

    service-deploy shows service endpoint after deploy

--- service-deploy.sh
+++ service-deploy.sh
@@ -4,3 +4,5 @@ app=$2
 fn=$3
 
 cd $workingdir && fn create app $app && fn -v build && fn deploy --app $app --local
+
+echo "http://localhost:8080/invoke/"$(fn inspect function $app $(fn list functions $app --output json | jq -r '.[].name') | jq -r '.id')
